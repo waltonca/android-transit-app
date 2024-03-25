61e42a5a8e166edf999907430cfb4c40f7a27f66
walton <zjxcdut@gmail.com>
2024-03-25 12:12:06 -0300

    Add scaffolding to display a bottom bar and add 3 screens

--- app/src/main/java/com/example/transitapp/MainActivity.kt
+++ app/src/main/java/com/example/transitapp/MainActivity.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.transitapp.screens.AlertScreen
+import com.example.transitapp.screens.MapScreen
+import com.example.transitapp.screens.RouteScreen
 import com.example.transitapp.ui.theme.TransitAppTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TransitApp()
                 }
             }
         }
@@ -30,17 +44,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TransitApp(){
+    
+    //test
+    //Text(text = "hello")
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TransitAppTheme {
-        Greeting("Android")
+    // Step 3: Add scaffolding to display a bottom app bar
+
+    // The NavHostController manages the screen navigation
+    val navController: NavHostController = rememberNavController()
+
+    //var textFieldLocation by remember { mutableStateOf("") }
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = { navController.navigate("map") }) {
+                        Icon(
+                            painterResource(id = R.drawable.baseline_map_24),
+                            contentDescription = "Map"
+                        )
+                    }
+                    IconButton(onClick = { navController.navigate("route") }) {
+                        Icon(
+                            painterResource(id = R.drawable.baseline_add_road_24),
+                            contentDescription = "Route"
+                        )
+                    }
+                    IconButton(onClick = { navController.navigate("alert") }) {
+                        Icon(
+                            painterResource(id = R.drawable.baseline_add_alert_24),
+                            contentDescription = "Alert"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        // NavHost will display the correct screen
+        NavHost(
+            navController = navController,
+            startDestination = "map",
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = "map") {
+                MapScreen()
+            }
+            composable(route = "route") {
+                RouteScreen()
+            }
+            composable(route = "alert") {
+                AlertScreen()
+            }
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/transitapp/screens/AlertScreen.kt
@@ -0,0 +1,9 @@
+package com.example.transitapp.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AlertScreen() {
+    Text("Alert Screen")
+}
