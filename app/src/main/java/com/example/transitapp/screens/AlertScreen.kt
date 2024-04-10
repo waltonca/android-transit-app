2d73014cd5a9aace0a92f543584e2d0dfeef4997
walton <zjxcdut@gmail.com>
2024-04-10 14:04:11 -0300

    Fixed some style in AlertScreen.

--- app/src/main/java/com/example/transitapp/screens/AlertScreen.kt
+++ app/src/main/java/com/example/transitapp/screens/AlertScreen.kt
@@ -2,19 +2,26 @@ package com.example.transitapp.screens
 
 import android.util.Log
 import android.widget.TextView
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.transitapp.MainViewModel
 import com.example.transitapp.R
@@ -68,6 +75,8 @@ fun DisplayAlerts(alert: GtfsRealtime.Alert) {
     val routeId = alert.informedEntityList.getOrNull(0)?.routeId ?: ""
     val timeStart = alert.activePeriodList.getOrNull(0)?.start ?:""
     val timeEnd = alert.activePeriodList.getOrNull(0)?.end ?:""
+    val effect = alert.effect ?:""
+    val description = alert.headerText.translationList.getOrNull(0)?.text ?:""
 
     // Log.i("AlertScreen", "time: ${timeStart.toString()}")
     Log.i("AlertScreen", "time: ${timeEnd.toString()}")
@@ -81,9 +90,20 @@ fun DisplayAlerts(alert: GtfsRealtime.Alert) {
         modifier = Modifier.padding(15.dp)
     )
     {
-        Text(routeId)
-        Text("Start: ${strTimeStart}")
-        Text("End: ${strTimeEnd}")
+        Column {
+            Row {
+                Text("Route: ${routeId}")
+                Spacer(modifier = Modifier.width(20.dp))
+                Text("Effect: ${effect}")
+            }
+            Row {
+                Text("Start: ${strTimeStart}")
+            }
+            Row {
+                Text("Description: ${description}")
+            }
+        }
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
     }
 }
 
